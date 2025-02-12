3506ce2a132f752260cb0098458a99041dc2db15
24paketik <87018761+24paketik@users.noreply.github.com>
2025-02-12 14:57:37 +0300

    Добавил функции

--- userHandler.go
+++ userHandler.go
@@ -7,7 +7,6 @@ import (
 )
 
 type User struct {
-	Id   int    `json:"id"`
 	Name string `json:"name"`
 }
 
@@ -28,6 +27,8 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
 		createUser(w, r)
 	case "PUT":
 		updateUser(w, r)
+	case "DELETE":
+		deleteUserById(w, r)
 	default:
 		http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
 	}
@@ -63,12 +64,40 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 
 // put
 func updateUser(w http.ResponseWriter, r *http.Request) {
+	var request struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
 
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user, exists := users[request.ID]; exists {
+		user.Name = request.Name
+		users[request.ID] = user
+		json.NewEncoder(w).Encode(user)
+	} else {
+		http.Error(w, "Пользователь не найден", http.StatusNotFound)
+	}
 }
 
 // delete by id
 func deleteUserById(w http.ResponseWriter, r *http.Request) {
+	var userId int
 
+	if err := json.NewDecoder(r.Body).Decode(&userId); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if _, exists := users[userId]; exists {
+		delete(users, userId)
+		response := map[string]string{"message": "Пользователь удален"}
+		json.NewEncoder(w).Encode(response)
+	} else {
+		http.Error(w, "Пользователя нет", http.StatusNotFound)
+	}
 }
 
 // delete all
